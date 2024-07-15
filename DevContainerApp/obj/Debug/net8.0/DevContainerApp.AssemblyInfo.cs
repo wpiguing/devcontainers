6af83d413bfcaf349a54834ec29805aae16350ec
wilson.piguing <wilson.piguing@northwestern.edu>
2024-07-15 19:43:19 +0000

    added gitignore

--- DevContainerApp/obj/Debug/net8.0/DevContainerApp.AssemblyInfo.cs
+++ DevContainerApp/obj/Debug/net8.0/DevContainerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevContainerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f63132f2dcbf113c08213767f8dbc17e0f3b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdf5b202e2ce768e1f156b58c94be4c4b480173e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevContainerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevContainerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
